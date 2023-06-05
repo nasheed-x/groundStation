b8da2f2c2a6504b909106f5bd5801ed201ebd1fa
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2023-06-05 16:26:09 +0400

    Removed TaskScheduler

--- src/config.h
+++ src/config.h
@@ -2,11 +2,14 @@
 #define CONFIG_H
 
 #include <Arduino.h>
-#include "rfm69.h"
-#include "TaskScheduler.h"
+#include <SPI.h>
+#include <RH_RF69.h>
 
-#define RFM69_INT 2
-#define RFM69_CS 10 
-#define RFM69_RST 9
+#define RF69_FREQ 915.0
+
+#define RFM69_CS    10
+#define RFM69_INT   2
+#define RFM69_RST   9
+#define LED        13
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,116 @@
 #include "config.h"
-#include "util.h"
 
-Transceiver *transceiver;
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-void setup()
-{
-    // Initialize communication
-    Serial.begin(115200);
+void setup() {
 
-    // Wait until serial console is open, remove if not tethered to computer
-    while (!Serial)
-    {
-        delay(1);
-    }
+  Serial.begin(115200);
 
-    if(Serial)
-    {
-        Serial.println("Serial Console Open.");
-    }
+  pinMode(LED, OUTPUT);
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
 
-    transceiver = new Transceiver(RFM69_CS, RFM69_INT);
+  while (!Serial)
+  {
+    delay(1);
+  }
 
-    if(check_sensors(transceiver))
-    {
-        Serial.println("Transceiver Connected.");
-    }
-    else
-    {
-        Serial.println("Transceiver Not Connected.");
-    }
+  if(Serial)
+  {
+    Serial.println("Serial Monitor Ready!");
+  }
+  else
+  {
+    Serial.println("Serial Monitor Not Ready!");
+  }
 
-    if(transceiver)
-    {
-        Serial.println("Transceiver Created.");
-    }
-    else
-    {
-        Serial.println("Transceiver Not Created.");
-    }
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
 
-    transceiver->enable();
+  if (!rf69.init()) {
+    Serial.println("RFM69 Radio init() Failed.");
+    while (1);
+  }
+  else
+  {
+    Serial.println("RFM69 Radio init() OK!");
+  }
 
-    if(transceiver->enable())
-    {
-        Serial.println("Transceiver Enabled.");
-    }
-    else
-    {
-        Serial.println("Transceiver Not Enabled.");
-    }
+  if (!rf69.setFrequency(RF69_FREQ)) 
+  {
+    Serial.println("setFrequency failed.");
+  }
+  else
+  {
+    Serial.println("setFrequency success.");
+  }
+
+  rf69.setTxPower(18, true);
+
+  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+
+  rf69.setEncryptionKey(key);
+
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
 
 }
 
-void loop()
-{
-    scheduler.execute();
+void loop() {
+
+    if (rf69.available())
+    {
+      uint8_t buf[120];
+      uint8_t len = sizeof(buf);
+
+      if (rf69.recv(buf, &len))
+      {
+          if (!len)
+          {
+            return;
+          }
+        Serial.println((char *)buf);
+        buf[len] = 0;
+
+        Serial.print("Signal Strength: ");
+        Serial.println(rf69.lastRssi(), DEC);
+        Serial.println("");
+      }
+    }
+
+    char data[RH_RF69_MAX_MESSAGE_LEN];
+    static char endMarker = '\n';
+    char receivedChar;
+    int ndx = 0;
+    memset(data, 32, sizeof(data));
+
+    bool dataReceived = false;
+
+    while (Serial.available() > 0)
+    {
+      receivedChar = Serial.read();
+      if (receivedChar == endMarker)
+      {
+        data[ndx] = '\0';
+        continue;
+      }
+
+      data[ndx] = receivedChar;
+      ndx++;
+      if (ndx >= RH_RF69_MAX_MESSAGE_LEN)
+      {
+        break;
+      }
+      dataReceived = true;
+    }
+
+    if (dataReceived)
+    {
+        rf69.send((uint8_t *)data, sizeof(data));
+        rf69.waitPacketSent();
+    }
+
+    memset(data, 32, sizeof(data));
 }
